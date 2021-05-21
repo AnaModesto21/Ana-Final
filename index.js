4bb57e167938f7a6be3074fb77ef9409e4ce6170
AnaModesto21 <77633954+AnaModesto21@users.noreply.github.com>
2021-05-21 14:28:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -38,6 +38,8 @@ function formatDate(date) {
        `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
      );
      iconElement.setAttribute("alt", response.data.weather[0].description);
+
+     celsiusTemperature = response.data.main.temp;
      }
   
   function showCity(event) {
