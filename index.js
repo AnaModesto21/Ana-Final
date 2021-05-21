df35c01691ada0ba81933db8bbcd78f7a3c8ce17
AnaModesto21 <77633954+AnaModesto21@users.noreply.github.com>
2021-05-21 13:20:23 +0100

    d

--- index.js
+++ index.js
@@ -29,7 +29,15 @@ function formatDate(date) {
     document.querySelector("#mainDegreesMax").innerHTML = Math.round(response.data.main.temp);
     document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed);
     document.querySelector("#type-Weather").innerHTML = response.data.weather[0].main;
-    document.querySelector("#icon").setAttribute = (`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    /*document.querySelector("#icon").setAttribute = (`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);*/
+
+
+    let iconElement = document.querySelector("#icon");
+    iconElement.setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+    iconElement.setAttribute("alt", response.data.weather[0].description);
     }
   
   
