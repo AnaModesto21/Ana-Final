1bc26948bce24b107624a3b196f205b9798e618d
AnaModesto21 <77633954+AnaModesto21@users.noreply.github.com>
2021-05-24 15:39:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -31,8 +31,7 @@ function formatDate(date) {
         days.forEach(function (day) {
         forecastHTML = forecastHTML + 
       `
-      <div class="col-2">
-      <div class="col-2">
+      
       <div class="weather-forecast-date">${day}</div>
       <div class="newIcon">
       <img src="http://openweathermap.org/img/wn/01d@2x.png" id="newIcon"/>
@@ -40,8 +39,7 @@ function formatDate(date) {
         <p class="card-text" id="max">
           <strong> 21º </strong> | <span id="min">16º</span>
         </p>
-      </div>
-      </div>
+     
       </div>`;
 });
 
