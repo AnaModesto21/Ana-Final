77697ed4932072ed91536dbd2250a8e74b92181c
AnaModesto21 <77633954+AnaModesto21@users.noreply.github.com>
2021-05-26 17:59:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ function formatDate(date) {
 
   function getForecast(coordinates) {
     console.log(coordinates);
-    let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    let apiKey = "68f3915a75420edb9d9f25f95e630a55";
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayForecast);
   }
