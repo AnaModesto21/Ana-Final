30e10210f28535c2678d2d5c4891510ece71c999
AnaModesto21 <77633954+AnaModesto21@users.noreply.github.com>
2021-05-26 18:08:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ function formatDate(date) {
      let iconElement = document.querySelector("#icon");
      iconElement.setAttribute(
        "src",
-       `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+       `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
      );
      iconElement.setAttribute("alt", response.data.weather[0].description);
   
