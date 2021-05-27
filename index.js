a15140f4f76d6030f4ba00e632328c3b4894379b
AnaModesto21 <77633954+AnaModesto21@users.noreply.github.com>
2021-05-27 15:40:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -45,8 +45,7 @@ function formatDate(date) {
       forecastHTML =
         forecastHTML +
         `
-        <div class="row">
-        <div class="col-2 weekday">
+        <div class="col-2 text-center">
         <div class="weather-forecast-date">${dateFormatting(forecastDate.dt)}</div>
         <div class="newIcon">
         <img
@@ -57,7 +56,6 @@ function formatDate(date) {
           <p class="card-text" id="max">
             <strong> ${forecastDate.temp.max} </strong> | <span id="min">${forecastDate.temp.min}</span>
           </p>
-        </div>
         </div>`;}
     });
     forecastHTML = forecastHTML + `</div>`;
